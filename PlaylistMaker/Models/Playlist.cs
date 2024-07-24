288623396c8d5c9f50281cff3ff10a48bf5b4223
Samantha Callie <Hoomee90@gmail.com>
2024-07-24 15:14:24 -0700

    add playlist create form and validation

--- PlaylistMaker/Models/Playlist.cs
+++ PlaylistMaker/Models/Playlist.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace PlaylistMaker.Models
 {
 	public class Playlist
 	{
 		public int PlaylistId { get; set; }
+		[Required(ErrorMessage = "The playlist must have a name.")]
 		public string Name { get; set; }
 		public List<Song> Songs { get; set; }
 	}
